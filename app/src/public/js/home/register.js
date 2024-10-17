45f884a6657f1a2a8a854627e32987c50da8699c
JUNE2001 <74485772+JUNE2001@users.noreply.github.com>
2024-10-17 14:33:16 +0900

    이용약관 업데이트

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,41 +1,52 @@
 "use strict";
 
-const id = document.querySelector('#id'),
-    psword = document.querySelector('#psword'),
-    confirmPsword = document.querySelector('#Confirm-psword'),
-    name = document.querySelector('#name'),
-    registerBtn = document.querySelector("#button");
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  confirmPsword = document.querySelector("#Confirm-psword"),
+  name = document.querySelector("#name"),
+  termsAgree = document.querySelector("#terms-agree"), // 이용약관 동의 체크박스
+  privacyAgree = document.querySelector("#privacy-agree"), // 개인정보처리방침 동의 체크박스
+  registerBtn = document.querySelector("#button");
 
 registerBtn.addEventListener("click", register);
 
-function register(){
-    if (!id.value) return alert("아이디를 입력해주십시오.");
-    if (psword.value !== confirmPsword.value) {
-        return alert("비밀번호가 일치하지 않습니다.")
-    }
-    const req = {
-        id: id.value,
-        psword: psword.value,
-        name: name.value,
-    };
+function register(event) {
+  event.preventDefault(); // 폼이 바로 제출되지 않도록 방지
 
-    fetch("/register", {
-        method: "Post",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(req),
-    })
+  if (!id.value) return alert("아이디를 입력해주십시오.");
+  if (psword.value !== confirmPsword.value) {
+    return alert("비밀번호가 일치하지 않습니다.");
+  }
+  if (!termsAgree.checked) {
+    return alert("이용약관에 동의해주십시오.");
+  }
+  if (!privacyAgree.checked) {
+    return alert("개인정보처리방침에 동의해주십시오.");
+  }
+
+  const req = {
+    id: id.value,
+    psword: psword.value,
+    name: name.value,
+  };
+
+  fetch("/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
     .then((res) => res.json())
-    .then((res) =>{
-        if (res.success) {
-            location.href = "/";
-        } else {
-            if (res.err) return alert(res.err);
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        if (res.err) return alert(res.err);
         alert(res.msg);
-        }
+      }
     })
     .catch((err) => {
-        console.error(new Error("회원가입 에러"))
+      console.error(new Error("회원가입 에러"));
     });
 }
